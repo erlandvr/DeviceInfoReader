446e6dd54007d7ec916f6f52860060677a3fedd2
erland <erland.vanreet@3factr.be>
2021-09-13 13:23:41 +0200

    #travelerFlow: "create Vhve project"

---
+++ VhveMatrix/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace VhveMatrix
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
