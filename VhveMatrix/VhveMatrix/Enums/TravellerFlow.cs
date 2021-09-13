0a7be8ef770dc5d43b81c69aa20d04e5ac53da49
erland <erland.vanreet@3factr.be>
2021-09-13 13:22:37 +0200

    #travelerFlow: "create enums"

---
+++ VhveMatrix/VhveMatrix/Enums/TravellerFlow.cs
@@ -0,0 +1,13 @@
+using VhveMatrix.VhveMatrix.Models.TravellerFlow.Flows;
+
+namespace VhveMatrix.VhveMatrix.Enums
+{
+	public enum TravellerFlow
+	{
+		DefaultFlow,
+		EU_CitizenFlow,
+		TCN_VisaExemptFlow,
+		TCN_VisaHolderFlow,
+		TCN_ResidencePermitFlow
+	}
+}
---
+++ VhveMatrix/VhveMatrix/Enums/TravellerTypeIdentifier.cs
@@ -0,0 +1,16 @@
+namespace VhveMatrix.VhveMatrix.Enums
+{
+	public enum TravellerTypeIdentifier
+	{
+		EU_Citizen= 1000,
+		TCN_VE_Residence_permit = 1002, //not found in matrix
+		TCN_VH_Residence_permit = 1003,
+		No_travel_document = 1005,
+		Auto = 6001,
+		TCN_VE_Default = 6002,
+		TCN_VE_Member_of_family = 6003,
+		TCN_VE_Right_of_free_movement = 6004,
+		TCN_VH_Default = 6005,
+		Facilitated_transit_document_holder = 6007, //TBD
+	}
+}
